db293789e50b8c9e1cf17bfd0619692c67cce422
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-04-28 05:35:19 +0700

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -7,7 +7,7 @@ class Config(object):
 
     CASH_API_KEY = ""  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
-    DATABASE_URL = postgres://ucfbwdaa:pIqm4jVFT_D58Zf-lhqcSt4k-2_a8nSp@salt.db.elephantsql.com/ucfbwdaa  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://ucfbwdaa:pIqm4jVFT_D58Zf-lhqcSt4k-2_a8nSp@salt.db.elephantsql.com/ucfbwdaa"  # A sql database url from elephantsql.com
 
     EVENT_LOGS = (-1001561812932)  # Event logs channel to note down important bot level events
 
