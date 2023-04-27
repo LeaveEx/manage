a2b7e40af14aac7127a44dcd7efaa44a01d4f37d
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-04-28 04:56:56 +0700

    Update afk.py

--- FallenRobot/modules/afk.py
+++ FallenRobot/modules/afk.py
@@ -60,14 +60,14 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                "{} is here!",
-                "{} is back!",
-                "{} is now in the chat!",
-                "{} is awake!",
-                "{} is back online!",
-                "{} is finally here!",
-                "Welcome back! {}",
-                "Where is {}?\nIn the chat!",
+                "{} Anak anjing kembali!",
+                "{} habis enak balik ketele!",
+                "{} habis putus ya hahah!",
+                "{} halo anjing!",
+                "{} balik ke tele pas sange doang!",
+                "{} ga ada yang nyari juga lu balik lgi!",
+                "halo anak kampang! {}",
+                "anjing {}?\nngapain balik!",
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
