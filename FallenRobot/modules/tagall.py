e6c20936a02d21337b7ccff3371835cfc378038b
LeaveEx <126212388+LeaveEx@users.noreply.github.com>
2023-04-28 05:04:45 +0700

    Update tagall.py

--- FallenRobot/modules/tagall.py
+++ FallenRobot/modules/tagall.py
@@ -56,7 +56,7 @@ async def mentionall(event):
         if not chat_id in spam_chats:
             break
         usrnum += 1
-        usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}), "
+        usrtxt += f"🐻‍❄️ [{usr.first_name}](tg://user?id={usr.id}), "
         if usrnum == 5:
             if mode == "text_on_cmd":
                 txt = f"{msg}\n{usrtxt}"
